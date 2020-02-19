b72340742a9025544e364bf3e72f24ae81906ae5
Celina F. Ribeiro <ribeiro.f.celina@gmail.com>
2020-02-19 15:47:25 -0300

    adiciona direção onda

--- xml.php
+++ xml.php
@@ -5,7 +5,7 @@ $xml = simplexml_load_file("http://servicos.cptec.inpe.br/XML/cidade/241/dia/0/o
 
 echo "Cidade: ".$xml->nome."<br/><br/>";
 echo "Altura das Ondas: <br/>";
-echo "Manhã: ".$xml->manha->altura."<br/>";
-echo "Tarde: ".$xml->tarde->altura."<br/>";
-echo "Noite: ".$xml->noite->altura."<br/>";
+echo "Manhã: ".$xml->manha->altura." - Direção ".$xml->manha->direcao."<br/>";
+echo "Tarde: ".$xml->tarde->altura." - Direção ".$xml->tarde->direcao."<br/>";
+echo "Noite: ".$xml->noite->altura." - Direção ".$xml->noite->direcao."<br/>";
 ?>
